451efb12786c9d07b23a2f2134754a257aed7bef
MushfiqMoon <mushfiq.moon013@google.com>
2023-05-31 01:05:40 +0600

    Palindrom Number done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 // 1. Two Sum 
 
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
 let twoSum = function (nums: number[], target: number): number[] | [] {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
@@ -10,3 +14,17 @@ let twoSum = function (nums: number[], target: number): number[] | [] {
     }
     return [];
 };
+
+// 9. Palindrome Number 
+
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+var isPalindrome = function (x: number): boolean {
+
+    let output = parseFloat(x.toString().split("").reverse().join(""))
+
+    return (output === x) ? true : false
+
+};
+
+isPalindrome(123)
