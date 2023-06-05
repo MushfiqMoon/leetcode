501e6a26c1b4c147838388a3be4d7d4f79c21c11
MushfiqMoon <mushfiq.moon013@google.com>
2023-06-06 00:19:22 +0600

    [solved]-704. Binary Search

--- src/index.ts
+++ src/index.ts
@@ -27,4 +27,37 @@ var isPalindrome = function (x: number): boolean {
 
 };
 
-isPalindrome(123)
+// 704. Binary Search
+
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function 
+// to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+function search(nums: number[], target: number): number {
+
+    let startIndex: number = 0
+    let endIndex: number = nums.length - 1
+    let midIndex: number
+    while (startIndex <= endIndex) {
+        midIndex = Math.floor((startIndex + endIndex) / 2)
+
+        if (nums[midIndex] === target) {
+            return midIndex
+        } else if (nums[midIndex] < target) {
+            startIndex = midIndex + 1
+        } else {
+            endIndex = midIndex - 1
+        }
+    }
+    return -1
+
+};
+
+let nums = [-1, 0, 3, 5, 9, 12]
+let target = 9
+
+console.log(search(nums, target))
+/*
+Input: 
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+*/
